ef3fc16c011370da539a4dab16429d37ffd86fc0
Deanna Swallow <swallowdeanna@gmail.com>
2019-03-09 14:33:29 -0700

    refactor routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Landing from './components/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Header />
-      </div>
+      <Router>
+        <div className="app">
+          <header>
+            <nav>
+              <Link to="/" className="nav-link">Deanna Swallow</Link>
+              <Link to="/about" className="nav-link">About</Link>
+              <Link to="/projects" className="nav-link">Projects</Link>
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </nav>
+          </header>
+          <main>
+            <Route exact path="/" component={Landing} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
