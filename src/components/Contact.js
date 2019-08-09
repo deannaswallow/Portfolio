0bc5c94272dfbb99b833bc2f0c093c6a5c7129f5
Deanna Swallow <swallowdeanna@gmail.com>
2019-08-09 08:59:55 -0600

    Adjust color scheme, center projects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,36 +1,52 @@
-import React from 'react';
+import React from "react";
 
 class Contact extends React.Component {
   render() {
     return (
       <div className="contact">
         <h1>Let's Chat!</h1>
-        <p>I love getting to know new people, discuss work opportunities, or
-        just talk about the latest episode of Black Mirror. If there's coffee
-        involved, even better. Or GIFs. I love GIFs. Pick your poison below.</p>
-        <a href="mailto:swallowdeanna@gmail.com" target="_blank"
-        rel="noopener noreferrer">
-          <span className="fas fa-envelope"></span>
-          <span>Email</span>
-        </a>
-        <a href="https://twitter.com/deannathedev" target="_blank"
-        rel="noopener noreferrer">
-          <span className="fab fa-twitter"></span>
-          <span>Twitter</span>
-        </a>
-        <a href="https://github.com/deannaswallow" target="_blank"
-        rel="noopener noreferrer">
-          <span className="fab fa-github"></span>
-          <span>GitHub</span>
-        </a>
-        <a href="https://www.linkedin.com/in/deanna-swallow/" target="_blank"
-        rel="noopener noreferrer">
-          <span className="fab fa-linkedin"></span>
-          <span>LinkedIn</span>
-        </a>
+        <p>
+          I love getting to know new people, discuss work opportunities, or just
+          talk about the latest episode of Black Mirror. If there's coffee
+          involved, even better. Or GIFs. I love GIFs. Pick your poison below.
+        </p>
+        <div className="contact-links">
+          <a
+            href="mailto:swallowdeanna@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="fas fa-envelope" />
+            <span>Email</span>
+          </a>
+          <a
+            href="https://twitter.com/deannathedev"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="fab fa-twitter" />
+            <span>Twitter</span>
+          </a>
+          <a
+            href="https://github.com/deannaswallow"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="fab fa-github" />
+            <span>GitHub</span>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/deanna-swallow/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="fab fa-linkedin" />
+            <span>LinkedIn</span>
+          </a>
+        </div>
       </div>
     );
   }
-};
+}
 
 export default Contact;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ class Projects extends React.Component {
     return (
       <div className="projects">
         <h1>My Projects</h1>
-        <p>
+        <p className="portfolio">
           This portfolio site is a project in and of itself, so{" "}
           <strong>
             <a
@@ -18,6 +18,7 @@ class Projects extends React.Component {
           </strong>{" "}
           to see the code for it.
         </p>
+        <br />
         <h2 className="project-title">Mario Kart Profiler</h2>
         <strong>
           <a
@@ -46,8 +47,14 @@ class Projects extends React.Component {
           </a>
         </strong>
         <p className="project-desc">
-          This is a project I built to demonstrate various skills including how
-          to use JSON.
+          This is a project I built to demonstrate various skills such as using
+          Create React App, JSON, and third party libraries. It was a lot of fun
+          to use a library to give it a nostalgic look. The biggest challenge I
+          faced was surpising to me: styling the radio controls. I felt that the
+          library's radio buttons weren't the best fit, so I set about to do my
+          own. It struck me as odd how difficult it was to give them a unique
+          look while making sure they were still accessible, which is a priority
+          to me.
         </p>
         <h2 className="project-title">Catch of the Day</h2>
         <strong>
@@ -67,7 +74,9 @@ class Projects extends React.Component {
         </strong>
         <p className="project-desc">
           This is a project I built during my quest to learn React. It
-          incorporates state, props, components, and routing.
+          incorporates state, props, components, and routing. The biggest
+          challenge I faced was managing information in state and making sure
+          that it went to the right places at the right time.
         </p>
         <h2 className="project-title">BlocJams</h2>
         <strong>
@@ -82,16 +91,6 @@ class Projects extends React.Component {
         <br />
         <strong>
           <a
-            href="https://github.com/deannaswallow/BlocJamsReact"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See the code for the React version here.
-          </a>
-        </strong>
-        <br />
-        <strong>
-          <a
             href="https://github.com/deannaswallow/bloc-jams-jquery"
             target="_blank"
             rel="noopener noreferrer"
@@ -100,45 +99,11 @@ class Projects extends React.Component {
           </a>
         </strong>
         <p className="project-desc">
-          There are actually three versions of this. I originally built it with
-          jQuery, then Angular, and then React. All three were done while I was
-          in a development boot camp. It's a music player app using some free
-          music files that are hosted online.
-        </p>
-        <h2 className="project-title">To Do App</h2>
-        <strong>
-          <a
-            href="https://github.com/deannaswallow/ReactToDo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See the code for the React version here.
-          </a>
-        </strong>
-        <br />
-        <strong>
-          <a
-            href="https://github.com/deannaswallow/angularjs-to-do"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See the code for the Angular version here.
-          </a>
-        </strong>
-        <br />
-        <strong>
-          <a
-            href="https://github.com/deannaswallow/to-do-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            See the code for the vanilla JavaScript version here.
-          </a>
-        </strong>
-        <p className="project-desc">
-          As was the case with BlocJams, there are three versions of this as
-          well: React, Angular, and vanilla JavaScript. Gotta know the basics,
-          right?
+          This is a music player app that I originally built in jQuery, then in
+          Angular. Doing it in those two formats helped me see the differences
+          between the two methods, which allowed me to see how each had their
+          own strengths. The biggest challenges I faced were making the controls
+          work correctly and making the progress bar update properly.
         </p>
       </div>
     );
